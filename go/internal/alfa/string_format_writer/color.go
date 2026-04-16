3856a82c76ace19b77411055e63c8616f8ef6057
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-04-16 07:54:12 -0400

    Trim dead code from string_format_writer
    Removed 8 unused formatter files (bracket_wrapped, date, func, delim,
    indented_header, prefix, string, quoted_string), dead status string
    constants, dead MakeColor generic, and unused color constants.
    
    Signed-off-by: Clown <https://github.com/amarbel-llc/clown>

--- go/internal/alfa/string_format_writer/color.go
+++ go/internal/alfa/string_format_writer/color.go
@@ -2,8 +2,6 @@ package string_format_writer
 
 import (
 	"github.com/amarbel-llc/madder/go/internal/0/fields"
-	"github.com/amarbel-llc/purse-first/libs/dewey/0/interfaces"
-	"github.com/amarbel-llc/purse-first/libs/dewey/bravo/errors"
 )
 
 func colorForType(t fields.Type) string {
@@ -23,63 +21,4 @@ func colorForType(t fields.Type) string {
 	default:
 		return colorNone
 	}
-}
-
-type color[T any] struct {
-	options            ColorOptions
-	color              fields.Type
-	stringFormatWriter interfaces.StringEncoderTo[T]
-}
-
-func MakeColor[T any](
-	o ColorOptions,
-	fsw interfaces.StringEncoderTo[T],
-	c fields.Type,
-) interfaces.StringEncoderTo[T] {
-	if o.OffEntirely {
-		return fsw
-	} else {
-		return &color[T]{
-			color:              c,
-			stringFormatWriter: fsw,
-		}
-	}
-}
-
-func (f *color[T]) EncodeStringTo(
-	e T,
-	sw interfaces.WriterAndStringWriter,
-) (n int64, err error) {
-	if f.options.OffEntirely {
-		return f.stringFormatWriter.EncodeStringTo(e, sw)
-	}
-
-	var n1 int
-
-	n1, err = sw.WriteString(colorForType(f.color))
-	n += int64(n1)
-
-	if err != nil {
-		err = errors.Wrap(err)
-		return n, err
-	}
-
-	var n2 int64
-	n2, err = f.stringFormatWriter.EncodeStringTo(e, sw)
-	n += n2
-
-	if err != nil {
-		err = errors.Wrap(err)
-		return n, err
-	}
-
-	n1, err = sw.WriteString(string(colorReset))
-	n += int64(n1)
-
-	if err != nil {
-		err = errors.Wrap(err)
-		return n, err
-	}
-
-	return n, err
 }
