9be2fb50effabae1def98715607549746ebff763
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-06-25 13:25:28 -0400

    bump nixpkgs-master: d233902 → 567a49d

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       inputs.treefmt-nix.follows = "bats/treefmt-nix";
     };
 
-    nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
 
     utils = {
       url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
